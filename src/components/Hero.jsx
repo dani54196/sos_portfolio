43b23e34aba0a1bffdeacfeda2fdb47ec9bab9b8
dani54196 <dani54196@gmail.com>
2024-04-25 11:17:16 -0300

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,54 +9,18 @@ const Hero = () => {
   const { t } = useTranslation();
 
   return (
-    <section id="home" className="overflow-hidden">
-      <div className="min-h-screen relative flex md:flex-row flex-col-reverse md:items-end justify-center items-center">
-        {/* <div
-          data-aos="slide-left"
-          data-aos-delay="1200"
-          className="absolute h-full md:w-4/12 w-8/12 top-0 right-0 bg-primaryLinear bottom-0 -z-10"
-        >
-          <h1 className="rotate-90 absolute top-[30%] right-[-15%] text-[#EAF2FA]">
-            {hero.firstName}{" "}
-            <span className="text-dark_primary">{hero.LastName}</span>
-          </h1>
-        </div> */}
-        {/* first col */}
-        <div className="pb-16 px-6 pt-5" data-aos="fade-down">
-          <h2>{hero.title}</h2>
-          <br />
-          <div className="flex justify-end">
-            <img className="" width={250} src={wp} />
-          </div>
-          <div className="flex flex-col gap-10 mt-10">
-            <div
-              data-aos="fade-down"
-              data-aos-delay={1 * 300}
-              className={"flex items-center w-80 gap-5"}
-            >
-              <h3>+3</h3>
-              <p>{t("hero.content1")}</p>
-            </div>
-            <div
-              data-aos="fade-down"
-              data-aos-delay={1 * 300}
-              className={" flex-row-reverse text-right"}
-            >
-              <h3>+30</h3>
-              <p>{t("hero.content2")}</p>
-            </div>
-          </div>
-        </div>
+    <section
+      id="home"
+      className="flex flex-col md:flex-row items-center justify-center"
+    >
+      <div className="md:w-1/2">
+        <img src={hero.image} alt="software of south logo" className="w-full p-5" />
+      </div>
 
-        {/* sec col */}
-        <div className="md:h-[37rem] h-96">
-          <img
-            src={hero.image}
-            data-aos="slide-up"
-            alt="software of south logo"
-            className="object-cover"
-          />
-        </div>
+      <div className="md:w-1/2 text-center md:text-left">
+        <h2 className="text-2xl md:text-4xl font-bold">{t("hero.title")}</h2>
+        <br />
+        <p className="text-lg md:text-xl">{t("hero.content")}</p>
       </div>
     </section>
   );
